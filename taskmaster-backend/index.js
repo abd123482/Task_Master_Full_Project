cba4953168983bc0cf57fb64127f282ec702355d
abd123482 <abdkkassl@gmail.com>
2025-08-14 20:59:26 +0200

    Update index.js

--- taskmaster-backend/index.js
+++ taskmaster-backend/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const taskRoutes = require("./routes/taskRoutes");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/tasks", taskRoutes);
