5d05e9248b893368c1365d1a05bb759bb02895c5
abd123482 <abdkkassl@gmail.com>
2025-08-14 14:14:50 +0200

    Update vite.config.js

--- Task_front_end/vite.config.js
+++ Task_front_end/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/Task_Master_Full_Project/',
+  plugins: [react()],
 })
